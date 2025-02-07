5010d1e84f8d28694973e8e3bc386a712dccc191
Danil Boenko <boenkodanil142@gmail.com>
2025-02-08 01:43:26 +0700

    Добавьте файлы проекта.

---
+++ CourseProject/NeuronType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    
+        public enum NeuronType
+        {
+            Input = 0,
+            Normal = 1,
+            Output = 2
+        }
+    
+}
---
+++ CourseProject/Topology.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public class Topology
+    {
+        public int InputCount { get; }//колл-во входных нейронов(первый слой)
+        public int OutputCount { get; }//колл-во выходных нейронов(последний слой)
+
+        public List<int> HiddenLayers { get; }
+
+        public Topology(int inputCount, int outputCount, params int[] layers)
+        {
+            InputCount = inputCount;
+            OutputCount = outputCount;
+            HiddenLayers = new List<int>();
+            HiddenLayers.AddRange(layers);
+        }
+    }
+}
